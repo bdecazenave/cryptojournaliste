566cb5bc18dbb58c733fb60e6163386cca5c7142
bdecazenave <benjamindecazenave@gmail.com>
2021-10-07 22:26:24 +0200

    commit de fin de journé relou

---
+++ lib/00_journalists.rb
@@ -0,0 +1,81 @@
+
+def start
+  journalistsnumber=["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+return journalistsnumber
+end
+
+# Ici je transforme mon tabeau en méthode. Je retourne mon tableau en variable afin de pouvoir l'exécuter par la suite dans mes méthodes.
+
+def array_length (journalistsnumber)
+puts "il y a #{journalistsnumber.count} journalistes dans ce fieffé coquin de tableau ?"
+end
+
+# Ici je vais créer une méthode qui va venir calculer le nombre d'entrées dans le tableau, c'est à dire, le nombre de journalistes. Je fais appel la variable à laquelle j'applique .count afin de compter les entrées dans le tableau
+
+def array_min (journalistsnumber)
+  puts "Le journaliste de ce maudit tableau ayant le nom le plus court est : #{journalistsnumber.min{|a, b| a.length <=> b.length }}"
+end
+
+# Ici je vais créer une méthode qui va venir identifier l'entrée ayant le nombre de caractères le plus faible du tableau. En comparant chacune des entrées entre elle (a,b) et en les éliminant, il finira par ressortir celui qui a le plus petit nombre de caractères
+
+def arrayfivecharactersnumber (journalistsnumber)
+  puts "Tu trouveras ci-dessous le nombre de journalistes dans ce vilain tableau dont le nom se compose de 5 caractères :  #{journalistsnumber.count {|a| a.length ==6}}"
+end
+
+# Ici j'applique un comptage (count) sur ma variable et je lui dis "sur chaque entrée, compte ceux dont l'entrée est égale à 6 caractères" (le ==6 permet de squizzer le @ que toutes les entrée du tableau ont)
+
+
+def arraystartswithcapital (journalistsnumber)
+  puts "Et comme tu es un petit curieux, sache qu'il y a #{journalistsnumber.count {|a| a =~ /^@[A-Z]/}} journalistes dont le nom commence par une lettre capitale dans ce fucking tableau."
+end
+
+# Ici j'applique un comptage (count) sur ma variable et je lui dis "sur chaque entrée, compte ceux dont l'entrée commence par une majuscule" (ce qui est entre crochet est une expréssion régulière qui dit "exclus le @" et retourne ceux qui ont une majuscule entre A et Z)
+
+def arraysortedalphabet (journalistsnumber)
+  puts " Sois pas relou et check ça --> tu trouveras tous les journalistes classés par ordre alphabétique : #{journalistsnumber.sort {|a,b| a.upcase<=>b.upcase}}"
+end
+
+#Ici j'applique un classement (sort) sur ma variable et je lui dis "compare la longueur de toutes les engtrées du tableau et classe les moi selon ce critère" (<=> permet de comparer. Cette fois a,b car on compare des choses avant on ne cherrypick qu'une entrée ou une liste d'entrées. upcase permet de ramener toutes les valeur sur une échelle commune pour les comparer) Est-ce que upcase permet au .sort de savoir que c'est un classement alphabétique qu'on veut ? et pourquoi il classe de à A à Z (en appliquant un .reverse sur .sort ca permet de classer en sens inverse. Donc la convention est "classe de A à Z")
+
+def arraysortedlength (journalistsnumber)
+  puts " Et la même liste cette fois si classée par taille de noms du plus petit au plus grand #{journalistsnumber.sort {|a,b| a.length<=>b.length}}"
+end
+
+#Ici j'applique un classement (sort) sur ma variable et je lui dis "compare la longueur de toutes les entrées du tableau et classe les moi" Je ne comprends pas comment il sait qu'il faut les classer du plus grand au plus petit (<=> permet de comparer. Cette fois a,b car on compare des choses avant on ne cherrypické qu'une entrée ou une liste d'entrées)
+
+def arrayepenserposition (journalistsnumber)
+  puts " Non sérieux tu connais un gars du tableau ? @epenser c'est ça ? Il est à la position #{journalistsnumber.index "@epenser"} du tableau."
+end
+
+#Ici j'applique un index sur ma variable, grosso modo ca veut dire "quelle est la position dans un tableau". Je lui spécifie l'entrée que je cherche et il me retourne sa position
+
+def arrayhandlerepartition (journalistsnumber)
+    puts " et pour finir, voilà la liste des journalistes classée par nombre de caractères #{journalistsnumber.group_by(&:length).map {|a, b| [a-1, b]}.sort}"
+end
+
+#Ici j'applique un groupement par nombre de caractères sur ma variable. Je demande à ce que ce soit regroupé dans un bloc avec .map (A REVOIR). Ensuite ca va chercher dans a le nbre de caractères - 1 pour enlever le @ du count), dans les positions, et ca les classe. Encore une fois comment sait il dans quel ordre classer ?
+
+def execute
+# c'est le début de ma méthode qui exécute toutes les autres
+  journalistsnumber = start
+# Permet de réutiliser la variable "journalistsnumber" dans chaque methode
+  array_length (journalistsnumber)
+# c'est la méthode 1
+  array_min (journalistsnumber)
+# c'est la méthode 2
+  arrayfivecharactersnumber (journalistsnumber)
+# c'est la méthode 3
+  arraystartswithcapital (journalistsnumber)
+# c'est la méthode 4
+  arraysortedalphabet (journalistsnumber)
+# c'est la méthode 5
+  arraysortedlength (journalistsnumber)
+  # c'est la méthode 6
+  arrayepenserposition (journalistsnumber)
+  # c'est la méthode 7
+  arrayhandlerepartition (journalistsnumber)
+  # c'est la méthode 8
+ end
+
+ execute
+# j'excute la derniere methode qui appellera toutes les autres
